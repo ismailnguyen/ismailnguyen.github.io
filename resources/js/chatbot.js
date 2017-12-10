1341f895c233763ac6ce705730144f02ffa0a835
Ismaïl <nguyen.ismail@gmail.com>
2017-12-10 20:57:45 +0100

    Refactor chatbot code

--- resources/js/chatbot.js
+++ resources/js/chatbot.js
@@ -11,66 +11,83 @@ $(function() {
         {
             return false;
         }
-		
-		// If user wants a game, redirect to flight game
-		if(msg.indexOf('game') >= 0)
-		{
-			window.location.replace('./flight/index.html');
-		}
 
-        // Print user request
-        print_message(msg, 'user');
+        // Handle user request
+        handle_message(msg);
 
         // Print bot response
         setTimeout(function() 
         {
-            print_message(msg, 'bot');
+            respond_message(msg);
         }, 1000); 
 
     });
 
-    function print_message(msg, type)
+    function handle_message(msg)
     {
         INDEX++;
-        var str="";
+        var str = "";
 
-        str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg "+type+"\">";
+        str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
         str += "<span class=\"msg-avatar\">";
 
-        if (type == 'bot')
-        {
-            str += "<img src=\"https://cdn0.iconfinder.com/data/icons/famous-characters-add-on-vol-2-glyph/48/Sed-23-128.png\">";
-        }
-        else
-        {
-            str += "<img src=\"https://cdn0.iconfinder.com/data/icons/famous-characters-add-on-vol-2-glyph/48/Sed-15-128.png\">";
-        }
+        str += "<img src=\"https://cdn0.iconfinder.com/data/icons/famous-characters-add-on-vol-2-glyph/48/Sed-15-128.png\">";
+
+        str += "<\/span>";
+        str += "<div class=\"cm-msg-text\">";
+        str += msg;
+
+        str += "<\/div>";
+        str += "<\/div>";
+
+        $("#chat-input").val('');
+  
+		print_message(str);
+    }
+	
+	function respond_message(msg)
+    {
+		// If user wants a game, redirect to flight game
+		let userWantsPlayGame = msg.indexOf('game') >= 0;
+		
+        INDEX++;
+        var str = "";
+
+        str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg bot\">";
+        str += "<span class=\"msg-avatar\">";
+
+        str += "<img src=\"https://cdn0.iconfinder.com/data/icons/famous-characters-add-on-vol-2-glyph/48/Sed-23-128.png\">";
 
         str += "<\/span>";
         str += "<div class=\"cm-msg-text\">";
 
-        if (type == 'bot')
-        {
-            str += "I'm your father !";
-        }
-        else
-        {
-            str += msg;
-        }
+		if (userWantsPlayGame)
+		{
+			str += "Let's start !";
+		}
+		else
+		{
+			str += "I'm your father !";
+		}
 
         str += "<\/div>";
         str += "<\/div>";
 
-        $(".chat-logs").append(str);
-        $("#cm-msg-"+INDEX).hide().fadeIn(300);
-
-        if(type == 'user')
-        {
-            $("#chat-input").val(''); 
-        }
-
+		print_message(str);
+		
+		if(userWantsPlayGame)
+		{
+			window.location.replace('./flight/index.html');
+		}
+    }
+	
+	function print_message(message)
+	{
+		$(".chat-logs").append(message);
+        $("#cm-msg-" + INDEX).hide().fadeIn(300);
+		
         $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
-    }  
+	}
 
     $(".chat-box-toggle").click(function() {
         $(".chat-box").toggle('scale');
