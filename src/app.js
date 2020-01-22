d3f3a4d3b564c0e96ea7c055d88fb6ef19061901
Ismail NGUYEN <Ismail.NGUYEN@loccitane.com>
2020-01-22 14:30:11 +0100

    Add a basic service worker

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import App from './app.html'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
 const app = new App({
   target: document.body,
 })
