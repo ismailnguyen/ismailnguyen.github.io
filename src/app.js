60513f503f9c1edbb29898b3d24b647d4a8a63fc
Ismail NGUYEN <Ismail.NGUYEN@loccitane.com>
2020-02-26 14:15:11 +0100

    Remove service worker

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 import App from './app.html'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-}
-
 const app = new App({
   target: document.body,
 })
