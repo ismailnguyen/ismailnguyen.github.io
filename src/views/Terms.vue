9b5c2f42364ca5ae639d5294b0978748f38319be
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-12-12 23:19:59 +0100

    Replace Terms page design by the one used in License and Privacy

--- src/views/Terms.vue
+++ src/views/Terms.vue
@@ -1,46 +1,48 @@
 <template>
     <section class="hero is-fullheight">
         <div class="hero-body">
-            <div class="container is-fullhd handwritten-text has-text-justified has-text-grey-dark">
-                <h1 class="title is-spaced">
-                    Terms And Conditions of Use
-                </h1>
-                
-                <h2 class="subtitle">WTFPL - Do What the Fuck You Want to Public License</h2>
-                
-                <p>
-                    Choose Freedom. Do What the Fuck You Want to.
-                </p>
+            <div class="container is-fullhd">
+                <article class="message">
+                    <div class="message-body">
+                        <div class="content">
+                            <h1 class="title is-spaced">
+                                Terms And Conditions of Use
+                            </h1>
+                            
+                            <h2 class="subtitle">WTFPL - Do What the Fuck You Want to Public License</h2>
+                            
+                            <p>
+                                Choose Freedom. Do What the Fuck You Want to.
+                            </p>
 
-                <p>
-                    Copyright (C) {{ today }} Isma&iuml;l NGUYEN
-                </p>
+                            <p>
+                                Copyright (C) {{ today }} Isma&iuml;l NGUYEN
+                            </p>
 
-                <p>
-                    Everyone is permitted to copy and distribute verbatim or modified 
-                    copies of this license document, and changing it is allowed as long 
-                    as the name is changed.
-                </p>
-				
-				<p>
-					DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-					<br>
-					TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-				</p>
-				
-				<p>
-					0. You just DO WHAT THE FUCK YOU WANT TO.
-				</p>
+                            <p>
+                                Everyone is permitted to copy and distribute verbatim or modified 
+                                copies of this license document, and changing it is allowed as long 
+                                as the name is changed.
+                            </p>
+                            
+                            <p>
+                                DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                                <br>
+                                TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+                            </p>
+                            
+                            <p>
+                                0. You just DO WHAT THE FUCK YOU WANT TO.
+                            </p>
 
-                <p>
-                    <small>
-                        See more at <a href="www.wtfpl.net" target="_blank" rel="noopener">www.wtfpl.net</a>
-                    </small>
-                </p>
-
-                <figure class="profile-picture image is-pulled-right">
-                    <img src="/resources/images/profile.png" alt="Profile picture">
-                </figure>
+                            <p>
+                                <small>
+                                    See more at <a href="www.wtfpl.net" target="_blank" rel="noopener">www.wtfpl.net</a>
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                </article>
             </div>
         </div>
     </section>
